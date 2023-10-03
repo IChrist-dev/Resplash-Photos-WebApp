3f7a37d5a1fb195ea7920048829232d8159939eb
Ian Christian <W0480449@campus.nscc.ca>
2023-10-03 10:20:34 -0300

    First class entity framework

---
+++ ReSplash/Models/Photo.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace ReSplash.Models
+{
+    public class Photo
+    {
+        public int PhotoId { get; set; }
+
+        public string FileName { get; set; } = string.Empty;
+
+        [DataType(DataType.DateTime)]
+        public DateTime PublishDate { get; set; }
+
+        public int NumViews { get; set; }
+
+        public int NumDownloads { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+
+        User User { get; set; } = new User();
+
+        // List<Tag> PhotoTags
+        // Category
+
+    }
+}
+
+// nuget entityframework...sqlserver
+// nuget entityframework...tools
